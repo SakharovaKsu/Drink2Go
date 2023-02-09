ecb6c2295195c8b57a1b8ec41b5c1a34dc239bf3
SakharovaKsu <nagis38@gmail.com>
2023-02-10 00:40:12 +0300

    Доработала шапку в мобилке, прописала скрип для бургера

--- less/source/js/script.js
+++ less/source/js/script.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.nav');
+let navToggle = document.querySelector('.nav__button');
+
+navMain.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function () {
+    navMain.classList.toggle('nav--closed');
+    navMain.classList.toggle('nav--opened');
+});
