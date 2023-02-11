45971f34637821c9e02d48e55b8d9797d9ae31a1
SakharovaKsu <nagis38@gmail.com>
2023-02-12 00:05:48 +0300

    Проверка по критериям

--- less/source/js/script.js
+++ less/source/js/script.js
@@ -14,7 +14,7 @@ const map = L.map('map')
   .setView({
     lat: 59.968137,
     lng: 30.316272,
-  }, 10);
+  }, 16);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -41,7 +41,7 @@ const marker = L.marker(
 
 marker.addTo(map);
 
-Slider
+// Slider
 const swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   navigation: {
