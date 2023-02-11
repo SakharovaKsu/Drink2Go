8280f83528db8ba48a744e930b8dd52c0ef56dd9
SakharovaKsu <nagis38@gmail.com>
2023-02-11 18:13:26 +0300

    Перешла к библиотекам

--- less/source/js/script.js
+++ less/source/js/script.js
@@ -1,4 +1,36 @@
 import { main } from "./main.js";
-import "./map.js";
+import { slider } from "./slider.js";
 
 main();
+slider();
+
+const map = L.map('map')
+  .setView({
+    lat: 59.968137,
+    lng: 30.316272,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '../img/map-pin.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const marker = L.marker(
+  {
+    lat: 59.968137,
+    lng: 30.316272,
+  },
+  {
+    icon: mainPinIcon,
+  },
+);
+
+marker.addTo(map);
---
+++ less/source/js/slider.js
@@ -0,0 +1,14 @@
+// export const slider = function() {
+//   const swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
+// }
