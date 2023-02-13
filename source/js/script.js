3679447637d72f8c136b888872f46f9efb3b392f
SakharovaKsu <nagis38@gmail.com>
2023-02-13 16:04:40 +0300

    Добавила файлы другой библиотеки слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -52,7 +52,16 @@ function onContentLoaded() {
   );
 
   marker.addTo(map);
+
+  $(document).ready(function(){
+    $('.slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
 }
 
 window.addEventListener('DOMContentLoaded', onContentLoaded);
-
